a56f4af665ce4d77fb93da85947a50a5ceb7a144
Octavio <valenzi.octavio@gmail.com>
2024-05-19 16:43:32 -0300

    Agregando cosas al proyecto en la rama master

--- Codigo/proyecto.py
+++ Codigo/proyecto.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 from pathlib import Path
 
+#INICIO EJERCICIO 1
 
 def levantar_imagen(img_path):
     if not img_path.exists():
@@ -10,29 +11,37 @@ def levantar_imagen(img_path):
     b,g,r = cv.split(aux)
     return aux,b,g,r
 
-#INICIO EJERCICIO 1
 
 #Inicializacion de variables
-prom_p = np.array([[1/3,1/3,1/3],[0.3,0.59,0.11]],dtype=np.float32)
-eleccion_img =  (input('Ingrese nombre de una imagen junto con su formato que se encuentre dentro de la carpeta Imagenes:\n'))
-elec_g = int(input('Seleccione escala de grises: 1-Promedio, 2-Promedio pesado \n'))
+prom_p = np.array([[1/3,1/3,1/3],[0.3,0.59,0.11]], dtype=np.float32)
+eleccion_img =  input('Ingrese nombre de una imagen, que se encuentre dentro de la carpeta Imagenes, junto con su formato:\n')
+elec_g = int(input('Seleccione el tipo de conversion a escala de grises segun el respectivo numero:\n1-Promedio, 2-Promedio pesado\n'))
 
 #Levantando la imagen
+
+"""La utilizacion de la libreria pathlib y, especificamente, del modulo Path es para dar robustez a la ruta de 
+la imagen debido a que la ruta de archivos no se determina de igual manera en todos los sistemas operativos.
+Path entonces se especializa en establecer la ruta de un archivo segun le corresponde al sistema operativo de la
+computadora en donde se este ejecutando el programa (por ejemplo Windows o Linux)."""
+
 image_path = (Path(f'/ProyectoPython/Imagenes/{eleccion_img}'))
 img,b,g,r = levantar_imagen(image_path)
 
 #Procesando imagen, operación generalizada para distintas escalas de grises
+
 """El calculo se realiza con las operaciones entre matrices ya integradas en la libreria
 numpy debido a que se encuentran optimizadas. El algoritmo de multiplicación por un escalar es
 de orden O(n) debido a que la matriz se guarda en posiciones de memoria contiguas, teniendo
 así que recorrer un "arreglo de una dimension" para realizar el cálculo."""
-img_gris=r*prom_p[elec_g-1,0] + g*prom_p[elec_g-1,1] + b*prom_p[elec_g-1,2]  
+
+img_gris= r*prom_p[elec_g-1,0] + g*prom_p[elec_g-1,1] + b*prom_p[elec_g-1,2]  
 img_gris = img_gris.astype(np.uint8)
 
 #Mostrando imagen
 cv.imshow("imgColor",img)
 cv.imshow("imgGris", img_gris)
 cv.imwrite("Imagenes/Computadora_grises.jpg",img_gris)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
 
--- Codigo/proyecto2.py
+++ Codigo/proyecto2.py
@@ -1,14 +1,17 @@
 import cv2 as cv
 import numpy as np
 import copy
+from pathlib import Path
 
 #INICIO EJERCICIO 2
 
-#FALTA:
-#GENERALIZAR LA ENTRADA DEL KERNEL
-
-#Ya realizado:
-#GENERALIZAR LA CREACION DE y (creo que se haria multiplicando a cada numero del slicing por kernel.shape[0]//2)
+def levantar_imagen_grayscale(image_path):
+    if not image_path.exists():
+        image_path = input("Hubo un error en el reconocimiento de la ruta de la imagen. Por favor ingrese la ruta completa de la imagen:\n")
+    
+    aux = cv.imread(str(image_path), cv.IMREAD_GRAYSCALE)
+    return aux
+    
 
 #Inicio de cambios
 Desenfoque_Gaussiano = (1 / 256.0)*np.array([
@@ -45,37 +48,48 @@ dic_kernels = {
     'Deteccion Ejes Horizontales' : Deteccion_Horizontal,
     'Sharpen' : Sharpen
 }
+
+
 lista_keys = list(dic_kernels.keys())
 """Terminar input del usuario, la idea era que la persona elija de manera
 ennumerada cual de los kernels quiere y en base a ese numero elegido acceder a la key en
 esa posicion y con esa key acceder a la matriz. El print de las opciones se puede hacer
 con un for in dic_kernels.key() o algo así"""
 
-d = cv.imread('/home/joaquin/Universidad/Python/ProyectoPython/Imagenes/Computadora_grises.jpg',cv.IMREAD_GRAYSCALE)  #Es necesario el segundo parametro para que la imagen no se cargue con 3 canales (daria error)
-#Fin de cambios
-x = np.array([
-    [1,2,3,90],
-    [4,5,6,90],
-    [7,8,9,90],
-    [1,2,3,90]
-])
+image_path = Path('/ProyectoPython/Imagenes/Computadora_grises.jpg')
 
+d = levantar_imagen_grayscale(image_path) 
+
+mascara_booleana = np.zeros(d.shape, dtype=bool)
+
+
+#Fin de cambios
 
 kernel = dic_kernels['Deteccion Ejes Horizontales']#Modifiqué
 
 coef = kernel.shape[0]//2
 
-d_copy = copy.deepcopy(d)   #esto genera una copia completa de la matriz a la cual le aplicaremos el kernel
-y = d_copy[1*coef:-1*coef,1*coef:-1*coef]   #esto hace que Y apunte solo al sector de d sin los bordes (hay que modificarlo para hacerlo generico)
+d_copy = copy.deepcopy(d)   #esto genera una copia completa de la matriz, copia a la cual le aplicaremos el kernel
 
+y = d_copy[coef:-coef,coef:-coef]   #Esto hace que la variable y sea la submatriz que no incluye los bordes de la matriz original, segun corresponda para cada kernel
+
+mascara_booleana[:coef,:] = True
+mascara_booleana[-1:-coef-1:-1,:] = True
+mascara_booleana[:,:coef] = True
+mascara_booleana[:,-1:-coef-1:-1] = True
+
+bordes = int(input("Indique mediante el respectivo numero que desea hacer con los bordes en la imagen con el filtro:\n1-Poner los bordes en negro, 2-Poner los bordes en blanco, 3-Mantener los bordes\n"))
 
 #De esta forma se puede hacer el kernel e ignorar los bordes
 for indice,valor in (np.ndenumerate(y)):
     y[indice]= np.sum(d[indice[0]:indice[0] + kernel.shape[0], indice[1]:indice[1] + kernel.shape[0]]*kernel)
       
-print(d_copy)
+if bordes==1 or bordes==2:
+    valores = [0,255]
+    d_copy[mascara_booleana]=valores[bordes-1]
+
 cv.imshow("ImgAlterada",d_copy)
-cv.imshow('ImgOriginal',d)#Modifiqué
+cv.imshow('ImgOriginal',d)
 cv.waitKey(0)
 
 cv.destroyAllWindows()
