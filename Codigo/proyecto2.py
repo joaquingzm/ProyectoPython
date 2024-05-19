558894e0931fd4b7a2eecf9d676355e17ead9c18
Joaquín Guzmán <joaquin.gzm234@gmail.com>
2024-05-19 14:12:52 -0300

    Terminando punto 2

--- Codigo/proyecto2.py
+++ Codigo/proyecto2.py
@@ -10,8 +10,49 @@ import copy
 #Ya realizado:
 #GENERALIZAR LA CREACION DE y (creo que se haria multiplicando a cada numero del slicing por kernel.shape[0]//2)
 
-d = cv.imread('D:\\ProyectoPython\\Imagenes\\Computadora_grises.jpg',cv.IMREAD_GRAYSCALE)  #Es necesario el segundo parametro para que la imagen no se cargue con 3 canales (daria error)
+#Inicio de cambios
+Desenfoque_Gaussiano = (1 / 256.0)*np.array([
+    [1, 4, 6, 4, 1],
+    [4, 16, 24, 16, 4],
+    [6, 24, 36, 24, 6],
+    [4, 16, 24, 16, 4],
+    [1, 4, 6, 4, 1]
+])
+Desenfoque_Normalizado = (1/9)*np.array([
+    [1,1,1],
+    [1,1,1],
+    [1,1,1]
+])
+Deteccion_Vertical = np.array([
+    [0.25,0,-0.25],
+    [0.5,0,-0.5],
+    [0.25,0,-0.25]
+])
+Deteccion_Horizontal = np.array([
+    [0.25,0.5,0.25],
+    [0,0,0],
+    [-0.25,-0.5,-0.25]    
+])
+Sharpen = np.array([
+    [0,-1,0],
+    [-1,5,-1],
+    [0,-1,0]
+])
+dic_kernels = {
+    'Desenfoque Gaussiano' : Desenfoque_Gaussiano,
+    'Desenfoque Normalizado' : Desenfoque_Normalizado,
+    'Deteccion Ejes Verticales' : Deteccion_Vertical,
+    'Deteccion Ejes Horizontales' : Deteccion_Horizontal,
+    'Sharpen' : Sharpen
+}
+lista_keys = list(dic_kernels.keys())
+"""Terminar input del usuario, la idea era que la persona elija de manera
+ennumerada cual de los kernels quiere y en base a ese numero elegido acceder a la key en
+esa posicion y con esa key acceder a la matriz. El print de las opciones se puede hacer
+con un for in dic_kernels.key() o algo así"""
 
+d = cv.imread('/home/joaquin/Universidad/Python/ProyectoPython/Imagenes/Computadora_grises.jpg',cv.IMREAD_GRAYSCALE)  #Es necesario el segundo parametro para que la imagen no se cargue con 3 canales (daria error)
+#Fin de cambios
 x = np.array([
     [1,2,3,90],
     [4,5,6,90],
@@ -20,11 +61,7 @@ x = np.array([
 ])
 
 
-kernel = (1 / 256.0) * np.array([[1, 4, 6, 4, 1],
-                                   [4, 16, 24, 16, 4],
-                                   [6, 24, 36, 24, 6],
-                                   [4, 16, 24, 16, 4],
-                                   [1, 4, 6, 4, 1]])
+kernel = dic_kernels['Deteccion Ejes Horizontales']#Modifiqué
 
 coef = kernel.shape[0]//2
 
@@ -38,7 +75,7 @@ for indice,valor in (np.ndenumerate(y)):
       
 print(d_copy)
 cv.imshow("ImgAlterada",d_copy)
-
+cv.imshow('ImgOriginal',d)#Modifiqué
 cv.waitKey(0)
 
 cv.destroyAllWindows()
